fe7cd5916eaaae980732dbb152a89fae463d51fc
Kevin <quebin31@gmail.com>
2020-02-10 22:39:29 -0500

    Return code and print error.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 mod error;
+mod logger;
 mod metadata;
 mod muso;
 
-use std::error::Error as StdError;
+use std::process;
 
 use clap::clap_app;
+use log::error;
 
+use crate::logger::init_logger;
 use crate::muso::Muso;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 const ABOUT: &str = env!("CARGO_PKG_DESCRIPTION");
 
-fn main() -> Result<(), Box<dyn StdError>> {
+fn main() {
+    init_logger().unwrap();
+
     let matches = clap_app! { muso =>
         (version: VERSION)
         (author: AUTHORS)
@@ -21,11 +26,17 @@ fn main() -> Result<(), Box<dyn StdError>> {
         (@arg watch: -w --watch "Run in watcher mode")
         (@arg format: -f --format +takes_value "Specifies format to save")
         (@arg dryrun: -d --dryrun "Don\'t organize files but show created paths")
-        (@arg config: -c --config +takes_value "Custom config file location")
+        (@arg config: -C --config +takes_value "Custom config file location")
+        (@arg recursive: -r --recursive "Perform sorting recursively")
+        (@arg clean: -c --clean "Clean empty folders at the end")
     }
     .get_matches();
 
-    Muso::new(&matches)?.run()?;
-    println!("Done!");
-    Ok(())
+    process::exit(match Muso::run(&matches) {
+        Ok(_) => 0,
+        Err(e) => {
+            error!("{}", e);
+            1
+        }
+    })
 }
