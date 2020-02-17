9d64086cc19109e23fc970506d0127d4c78e41c9
Kevin <quebin31@gmail.com>
2020-02-17 17:54:58 -0500

    Using action to set up ssh key.

--- .github/actions/push_pkgbuild/entrypoint.sh
+++ .github/actions/push_pkgbuild/entrypoint.sh
@@ -1,5 +1,4 @@
 target=$1
-sshkey=$2
 
 # '/github/workspace' is mounted as a volume and has owner set to root
 # set the owner to the 'build' user, so it can access package files
@@ -9,9 +8,6 @@ cd "$target"
 
 namcap PKGBUILD && makepkg --printsrcinfo > .SRCINFO
 
-echo $sshkey > ssh.key
-chmod 400 ssh.key
-git config --local core.sshCommand "ssh -i $(pwd)/ssh.key -F /dev/null -o StrictHostKeyChecking=no"
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git commit -m "Updated from actions" -a
