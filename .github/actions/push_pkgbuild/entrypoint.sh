659443b9dbb9416e6d0bdfee8777e60807a537c0
Kevin <quebin31@gmail.com>
2020-02-17 17:07:01 -0500

    Init ssh key on checkout submodule

--- .github/actions/push_pkgbuild/entrypoint.sh
+++ .github/actions/push_pkgbuild/entrypoint.sh
@@ -7,10 +7,9 @@ sudo chown -R build /github/workspace
 
 cd "$target"
 
-echo $sshkey > ssh.key
-
 namcap PKGBUILD && makepkg --printsrcinfo > .SRCINFO
 
+echo $sshkey > ssh.key
 git config --local core.sshCommand "ssh -i $(pwd)/ssh.key -F /dev/null"
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
