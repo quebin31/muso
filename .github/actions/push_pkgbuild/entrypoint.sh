9fa56720b8da007699b09862431fec27e0ca2173
Kevin <quebin31@gmail.com>
2020-02-17 17:01:04 -0500

    Changed action name.

--- .github/actions/push_pkgbuild/entrypoint.sh
+++ .github/actions/push_pkgbuild/entrypoint.sh
@@ -1,4 +1,5 @@
 target=$1
+sshkey=$2
 
 # '/github/workspace' is mounted as a volume and has owner set to root
 # set the owner to the 'build' user, so it can access package files
@@ -6,8 +7,12 @@ sudo chown -R build /github/workspace
 
 cd "$target"
 
+echo $sshkey > ssh.key
+
 namcap PKGBUILD && makepkg --printsrcinfo > .SRCINFO
 
+git config --local core.sshCommand "ssh -i $(pwd)/ssh.key -F /dev/null"
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git commit -m "Updated from actions" -a
+git push origin master
