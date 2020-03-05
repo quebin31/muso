d032c27231465be798adeb4122d0ae134a3ea5e2
Kevin <quebin31@gmail.com>
2020-03-05 13:56:56 -0500

    Rename prefix, more consisten messages.

--- src/logger.rs
+++ src/logger.rs
@@ -33,9 +33,9 @@ impl Log for MusoLogger {
 
     fn log(&self, record: &Record) {
         match record.level() {
-            Level::Info => println!("{} {}", Cyan.bold().paint("[info]"), record.args()),
-            Level::Warn => eprintln!("{} {}", Yellow.bold().paint("[warn]"), record.args()),
-            Level::Error => eprintln!("{} {}", Red.bold().paint("[error]"), record.args()),
+            Level::Info => println!("{} {}", Cyan.bold().paint("[i]"), record.args()),
+            Level::Warn => println!("{} {}", Yellow.bold().paint("[w]"), record.args()),
+            Level::Error => println!("{} {}", Red.bold().paint("[e]"), record.args()),
             _ => {}
         }
     }
