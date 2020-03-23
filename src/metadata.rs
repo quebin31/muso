90ca01e713752d5f8f26732d64dd1c9a2bc84b51
Kevin <quebin31@gmail.com>
2020-03-23 12:20:50 -0500

    Added support for ogg files. Removed build_path method, moved to ParsedFormat. Custom getters for tags. Create unit tests instead.

--- src/metadata.rs
+++ src/metadata.rs
@@ -15,12 +15,12 @@
 // You should have received a copy of the GNU General Public License
 // along with muso.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-use crate::error::{MusoError, Result};
-use crate::format::{Component, Placeholder};
+use crate::error::{AnyResult, MusoError, MusoResult};
 
 #[derive(Debug)]
 pub struct Metadata {
@@ -32,37 +32,36 @@ pub struct Metadata {
     pub ext: String,
 }
 
-macro_rules! get_placeholder {
-    ($self:ident, $placeholder:ident, $exfat_compat:expr) => {{
-        replace(
-            &$self
-                .$placeholder
-                .as_ref()
-                .ok_or_else(|| MusoError::MissingTag {
-                    tag: stringify!($placeholder).into(),
-                })?
-                .to_string(),
-            $exfat_compat,
-        )
-    }};
+macro_rules! impl_tag_getter {
+    ($self:ident, $tag:ident) => {
+        $self
+            .$tag
+            .as_ref()
+            .ok_or_else(|| MusoError::MissingTag {
+                tag: stringify!($tag).into(),
+            })
+            .map(|s| s.to_string())
+    };
 }
 
 impl Metadata {
-    pub fn from_path(path: impl AsRef<Path>) -> Result<Self> {
+    pub fn from_path(path: impl AsRef<Path>) -> AnyResult<Self> {
         let mut file = File::open(&path)?;
         let mut magic_bytes = [0; 4];
-        file.read_exact(&mut magic_bytes)?;
+        file.read_exact(&mut magic_bytes)
+            .map_err(|_| MusoError::NotSupported)?;
 
         let infer = infer::Infer::new();
         let ftype = infer.get(&magic_bytes).ok_or(MusoError::NotSupported)?;
         match ftype.mime.as_str() {
-            "audio/x-flac" => Metadata::from_vorbis(&path),
+            "audio/x-flac" => Metadata::from_flac_vorbis(&path),
             "audio/mpeg" => Metadata::from_id3(&path),
+            "audio/ogg" => Metadata::from_ogg_vorbis(&path),
             _ => Err(MusoError::NotSupported.into()),
         }
     }
 
-    fn from_id3(path: impl AsRef<Path>) -> Result<Self> {
+    fn from_id3(path: impl AsRef<Path>) -> AnyResult<Self> {
         let tag = id3::Tag::read_from_path(path)?;
 
         let artist = if let Some(artist) = tag.album_artist() {
@@ -86,13 +85,27 @@ impl Metadata {
         })
     }
 
-    fn from_vorbis(path: impl AsRef<Path>) -> Result<Self> {
+    fn from_flac_vorbis(path: impl AsRef<Path>) -> AnyResult<Self> {
         let tag = metaflac::Tag::read_from_path(path)?;
-        let comments = &tag
+        let comments = tag
             .vorbis_comments()
             .ok_or(MusoError::EmptyComments)?
-            .comments;
+            .comments
+            .to_owned();
 
+        Self::from_vorbis_comments(comments, "flac")
+    }
+
+    fn from_ogg_vorbis(path: impl AsRef<Path>) -> AnyResult<Self> {
+        let file = File::open(path)?;
+        let mut reader = ogg::reading::PacketReader::new(file);
+        let ((_, comments, _), _) = lewton::inside_ogg::read_headers(&mut reader)?;
+        let comments = Self::ogg_comment_map(comments.comment_list);
+
+        Self::from_vorbis_comments(comments, "ogg")
+    }
+
+    fn from_vorbis_comments(comments: HashMap<String, Vec<String>>, ext: &str) -> AnyResult<Self> {
         let artist = if let Some(artist) = comments.get("ALBUMARTIST").and_then(|a| a.get(0)) {
             Some(artist.to_owned())
         } else {
@@ -130,143 +143,134 @@ impl Metadata {
             disc,
             track,
             title,
-            ext: "flac".to_owned(),
+            ext: ext.to_owned(),
         })
     }
 
-    pub fn build_path(
-        &self,
-        parsed_format: &[Component],
-        exfat_compat: bool,
-    ) -> std::result::Result<String, MusoError> {
-        let mut path = String::with_capacity(64);
+    fn ogg_comment_map(list: Vec<(String, String)>) -> HashMap<String, Vec<String>> {
+        let mut map = HashMap::new();
 
-        for component in parsed_format {
-            match component {
-                Component::String(s) => path.push_str(&s),
-                Component::Placeholder(p) => {
-                    let value = match p {
-                        Placeholder::Artist => get_placeholder!(self, artist, exfat_compat),
-                        Placeholder::Album => get_placeholder!(self, album, exfat_compat),
-                        Placeholder::Disc { leading } => {
-                            let value = get_placeholder!(self, disc, exfat_compat);
-                            add_zeros(value, *leading)
-                        }
-                        Placeholder::Track { leading } => {
-                            let value = get_placeholder!(self, track, exfat_compat);
-                            add_zeros(value, *leading)
-                        }
-                        Placeholder::Title => get_placeholder!(self, title, exfat_compat),
-                        Placeholder::Ext => self.ext.clone(),
-                    };
-
-                    path.push_str(&value);
-                }
-                _ => unreachable!(),
-            }
+        for (key, value) in list {
+            let entry = map.entry(key).or_insert_with(|| vec![]);
+            entry.push(value);
         }
 
-        Ok(path)
+        map
     }
-}
 
-fn replace(string: &str, exfat_compat: bool) -> String {
-    if exfat_compat {
-        string
-            .replace('/', "_")
-            .replace('"', "_")
-            .replace('*', "_")
-            .replace(':', "_")
-            .replace('<', "_")
-            .replace('>', "_")
-            .replace('\\', "_")
-            .replace('?', "_")
-            .replace('|', "_")
-    } else {
-        string.replace('/', "_")
+    pub fn get_artist(&self) -> MusoResult<String> {
+        impl_tag_getter!(self, artist)
     }
-}
 
-fn add_zeros(string: String, leading: u8) -> String {
-    if (leading as usize) > string.len() {
-        let mut res: String = vec!['0'; leading as usize - string.len()].iter().collect();
-        res.push_str(&string);
-        res
-    } else {
-        string
+    pub fn get_album(&self) -> MusoResult<String> {
+        impl_tag_getter!(self, album)
+    }
+
+    pub fn get_disc(&self) -> MusoResult<String> {
+        impl_tag_getter!(self, disc)
+    }
+
+    pub fn get_track(&self) -> MusoResult<String> {
+        impl_tag_getter!(self, track)
+    }
+
+    pub fn get_title(&self) -> MusoResult<String> {
+        impl_tag_getter!(self, title)
+    }
+
+    pub fn get_ext(&self) -> String {
+        self.ext.clone()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::format::parse_format_string;
 
     #[test]
-    fn complete_flac_with_ok_format() {
+    fn complete_flac() {
         let metadata = Metadata::from_path("test_files/complete.flac").unwrap();
-        let format =
-            parse_format_string("{artist}/{album}/{disc}.{track} - {title}.{ext}").unwrap();
 
-        assert_eq! {
-            Ok("Album Artist/Album/1.1 - Title.flac".into()),
-            metadata.build_path(&format, false)
-        };
+        assert_eq!(Ok("Album Artist".into()), metadata.get_artist());
+        assert_eq!(Ok("Album".into()), metadata.get_album());
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("flac".to_owned(), metadata.get_ext());
     }
 
     #[test]
-    fn partial_flac_with_ok_format() {
+    fn partial_flac() {
         let metadata = Metadata::from_path("test_files/partial.flac").unwrap();
-        let format = parse_format_string("{artist}/{disc}.{track} - {title}.{ext}").unwrap();
 
-        assert_eq! {
-            Ok("Artist/1.1 - Title.flac".into()),
-            metadata.build_path(&format, false)
-        };
+        assert_eq!(Ok("Artist".into()), metadata.get_artist());
+        assert_eq!(
+            Err(MusoError::MissingTag {
+                tag: "album".into()
+            }),
+            metadata.get_album()
+        );
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("flac".to_owned(), metadata.get_ext());
     }
 
     #[test]
-    fn partial_flac_with_bad_format() {
-        let metadata = Metadata::from_path("test_files/partial.flac").unwrap();
-        let format = parse_format_string("{artist}/{album}").unwrap();
-
-        assert_eq! {
-            Err(MusoError::MissingTag{ tag: "album".into() }),
-            metadata.build_path(&format, false)
-        };
-    }
-
-    #[test]
-    fn complete_mp3_with_ok_format() {
+    fn complete_mp3() {
         let metadata = Metadata::from_path("test_files/complete.mp3").unwrap();
-        let format =
-            parse_format_string("{artist}/{album}/{disc}.{track} - {title}.{ext}").unwrap();
 
-        assert_eq! {
-            Ok("Album Artist/Album/1.1 - Title.mp3".into()),
-            metadata.build_path(&format, false)
-        };
+        assert_eq!(Ok("Album Artist".into()), metadata.get_artist());
+        assert_eq!(Ok("Album".into()), metadata.get_album());
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("mp3".to_owned(), metadata.get_ext());
     }
 
     #[test]
-    fn partial_mp3_with_ok_format() {
+    fn partial_mp3() {
         let metadata = Metadata::from_path("test_files/partial.mp3").unwrap();
-        let format = parse_format_string("{artist}/{disc}.{track} - {title}.{ext}").unwrap();
 
-        assert_eq! {
-            Ok("Artist/1.1 - Title.mp3".into()),
-            metadata.build_path(&format, false)
-        };
+        assert_eq!(Ok("Artist".into()), metadata.get_artist());
+        assert_eq!(
+            Err(MusoError::MissingTag {
+                tag: "album".into()
+            }),
+            metadata.get_album()
+        );
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("mp3".to_owned(), metadata.get_ext());
     }
 
     #[test]
-    fn partial_mp3_with_bad_format() {
-        let metadata = Metadata::from_path("test_files/partial.mp3").unwrap();
-        let format = parse_format_string("{artist}/{album}").unwrap();
+    fn complete_ogg() {
+        let metadata = Metadata::from_path("test_files/complete.ogg").unwrap();
 
-        assert_eq! {
-            Err(MusoError::MissingTag{ tag: "album".into() }),
-            metadata.build_path(&format, false)
-        };
+        assert_eq!(Ok("Album Artist".into()), metadata.get_artist());
+        assert_eq!(Ok("Album".into()), metadata.get_album());
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("ogg".to_owned(), metadata.get_ext());
+    }
+
+    #[test]
+    fn partial_ogg() {
+        let metadata = Metadata::from_path("test_files/partial.ogg").unwrap();
+
+        assert_eq!(Ok("Artist".into()), metadata.get_artist());
+        assert_eq!(
+            Err(MusoError::MissingTag {
+                tag: "album".into()
+            }),
+            metadata.get_album()
+        );
+        assert_eq!(Ok("1".into()), metadata.get_disc());
+        assert_eq!(Ok("1".into()), metadata.get_track());
+        assert_eq!(Ok("Title".into()), metadata.get_title());
+        assert_eq!("ogg".to_owned(), metadata.get_ext());
     }
 }
