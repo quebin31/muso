b668163b14739f5ea5299e0b3d890eb1d83e4e42
Kevin Del Castillo <quebin31@gmail.com>
2020-07-15 23:00:07 -0500

    Create tests automatically with macros

--- tests/format_string.rs
+++ tests/format_string.rs
@@ -1,86 +1,115 @@
-use std::str::FromStr;
+macro_rules! define_tests_for {
+    ($ext:ident) => {
+        #[cfg(test)]
+        mod $ext {
+            use std::str::FromStr;
 
-use muso::error::{AnyResult, MusoError};
-use muso::format::ParsedFormat;
-use muso::metadata::Metadata;
+            use muso::error::{AnyResult, MusoError};
+            use muso::format::ParsedFormat;
+            use muso::metadata::Metadata;
 
-fn complete_with_ok_format(ext: &str) -> AnyResult<()> {
-    let metadata = Metadata::from_path(format!("test_files/complete.{}", ext))?;
-    let format = ParsedFormat::from_str("{artist}/{album}/{disc}.{track} - {title}.{ext}")?;
+            #[test]
+            fn complete_with_ok_format() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/complete.{}", ext))?;
+                let format =
+                    ParsedFormat::from_str("{artist}/{album}/{disc}.{track} - {title}.{ext}")?;
 
-    assert_eq! {
-        Ok(format!("Album Artist/Album/1.1 - Title.{}", ext)),
-        format.build_path(&metadata, false)
+                assert_eq! {
+                    Ok(format!("Album Artist/Album/1.1 - Title.{}", ext)),
+                    format.build_path(&metadata, false)
+                };
+
+                Ok(())
+            }
+
+            #[test]
+            fn partial_with_ok_format() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
+                let format = ParsedFormat::from_str("{artist}/{disc}.{track} - {title}.{ext}")?;
+
+                assert_eq! {
+                    Ok(format!("Artist/1.1 - Title.{}", ext)),
+                    format.build_path(&metadata, false)
+                };
+
+                Ok(())
+            }
+
+            #[test]
+            fn both_with_ok_optional_format() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
+                let format = ParsedFormat::from_str("{artist}/{album?} - {title}.{ext}")?;
+
+                assert_eq! {
+                    Ok(format!("Artist/ - Title.{}", ext)),
+                    format.build_path(&metadata, false)
+                };
+
+                let metadata = Metadata::from_path(format!("test_files/complete.{}", ext))?;
+
+                assert_eq!(
+                    Ok(format!("Album Artist/Album - Title.{}", ext)),
+                    format.build_path(&metadata, false)
+                );
+
+                Ok(())
+            }
+
+            #[test]
+            fn bad_optional_formats() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
+                let format = ParsedFormat::from_str("{artist}/{album?}/{title}.{ext}")?;
+
+                assert_eq! {
+                    Err(MusoError::OptionalInDir),
+                    format.build_path(&metadata, false)
+                };
+
+                let format = ParsedFormat::from_str("{artist}/{title?}.{ext}")?;
+
+                assert_eq! {
+                    Err(MusoError::RequiredInFile),
+                    format.build_path(&metadata, false)
+                }
+
+                Ok(())
+            }
+
+            #[test]
+            fn bad_file_format() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
+                let format = ParsedFormat::from_str("{artist}/foo.{ext}")?;
+
+                assert_eq! {
+                    Err(MusoError::RequiredInFile),
+                    format.build_path(&metadata, false)
+                };
+
+                Ok(())
+            }
+
+            #[test]
+            fn partial_with_bad_format() -> AnyResult<()> {
+                let ext = stringify!($ext);
+                let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
+                let format = ParsedFormat::from_str("{artist}/{album}")?;
+
+                assert_eq! {
+                    Err(MusoError::MissingTag{ tag: "album".into() }),
+                    format.build_path(&metadata, false)
+                };
+
+                Ok(())
+            }
+        }
     };
-
-    Ok(())
 }
 
-fn partial_with_ok_format(ext: &str) -> AnyResult<()> {
-    let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
-    let format = ParsedFormat::from_str("{artist}/{disc}.{track} - {title}.{ext}")?;
-
-    assert_eq! {
-        Ok(format!("Artist/1.1 - Title.{}", ext)),
-        format.build_path(&metadata, false)
-    };
-
-    Ok(())
-}
-
-fn partial_with_bad_format(ext: &str) -> AnyResult<()> {
-    let metadata = Metadata::from_path(format!("test_files/partial.{}", ext))?;
-    let format = ParsedFormat::from_str("{artist}/{album}")?;
-
-    assert_eq! {
-        Err(MusoError::MissingTag{ tag: "album".into() }),
-        format.build_path(&metadata, false)
-    };
-
-    Ok(())
-}
-
-#[test]
-fn complete_flac_with_ok_format() -> AnyResult<()> {
-    complete_with_ok_format("flac")
-}
-
-#[test]
-fn partial_flac_with_ok_format() -> AnyResult<()> {
-    partial_with_ok_format("flac")
-}
-
-#[test]
-fn partial_flac_with_bad_format() -> AnyResult<()> {
-    partial_with_bad_format("flac")
-}
-
-#[test]
-fn complete_mp3_with_ok_format() -> AnyResult<()> {
-    complete_with_ok_format("mp3")
-}
-
-#[test]
-fn partial_mp3_with_ok_format() -> AnyResult<()> {
-    partial_with_ok_format("mp3")
-}
-
-#[test]
-fn partial_mp3_with_bad_format() -> AnyResult<()> {
-    partial_with_bad_format("mp3")
-}
-
-#[test]
-fn complete_ogg_with_ok_format() -> AnyResult<()> {
-    complete_with_ok_format("ogg")
-}
-
-#[test]
-fn partial_ogg_with_ok_format() -> AnyResult<()> {
-    partial_with_ok_format("ogg")
-}
-
-#[test]
-fn partial_ogg_with_bad_format() -> AnyResult<()> {
-    partial_with_bad_format("ogg")
-}
+define_tests_for!(flac);
+define_tests_for!(mp3);
+define_tests_for!(ogg);
